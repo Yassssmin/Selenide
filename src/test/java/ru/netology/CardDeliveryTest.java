e23e5e8978dd4314957daf1aefd826d55c60630f
anaaveranova <skillet88jou@hotmail.com>
2020-12-15 22:30:16 +0300

    Добавление теста, Задание 2

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
@@ -26,11 +29,6 @@ public class CardDeliveryTest {
     void clearDateField() {
         for (int i = 0; i < 10; i++) {
             $("input[type='tel']").toWebElement().sendKeys(Keys.BACK_SPACE);
-        }
-    }
-    void clearDateFieldEmpty() {
-        for (int i = 0; i < 10; i++) {
-            $("input[type='tel']").setValue("");
         }
     }
 
@@ -96,8 +94,7 @@ public class CardDeliveryTest {
     @Test
     void shouldCardDeliveryDataInvalid() {
         $("input[type='text']").setValue("Москва");
-        $("input[type='tel']").toWebElement().sendKeys(Keys.BACK_SPACE);
-        $("input[type='tel']").setValue("15.12.2020");
+        $("input[type='tel']").setValue(getInvalidDateString());
         $("input[name='name']").setValue("Василий Иванович");
         $("input[name='phone']").setValue("+79990873456");
         $("[data-test-id=agreement]").click();
@@ -182,5 +179,37 @@ public class CardDeliveryTest {
         $("input[name='phone']").setValue("+79990873456");
         $(withText("Забронировать")).click();
         $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
+    }
+    @Test
+    void shouldCardDeliverySuccessCityList() {
+        // Календарь
+        LocalDate dateNow = LocalDate.now();
+        LocalDate plusWeek = dateNow.plusWeeks(1);
+        LocalDate availableDay = dateNow.plusDays(3);
+
+        boolean isSameYear = dateNow.getYear() == plusWeek.getYear();
+        boolean isMonthDiff = dateNow.getMonthValue() < plusWeek.getMonthValue();
+        boolean availableStartSameMonth = availableDay.getMonthValue() == dateNow.getMonthValue();
+
+        boolean needToChangeMonth = availableStartSameMonth && (!isSameYear || isMonthDiff);
+
+        $("button[type='button']").click();
+
+        if (needToChangeMonth) {
+            $(".calendar__arrow_direction_right[data-step='1']").click();
+        }
+
+        long timestampInMs = plusWeek.toEpochSecond(LocalTime.MIDNIGHT, ZoneOffset.systemDefault().getRules().getOffset(Instant.now())) * 1000;
+
+        $(".calendar__day[data-day='" + timestampInMs + "']").click();
+
+        // Остальные поля
+        $("input[type='text']").setValue("Мо");
+        $(withText("Москва")).click();
+        $("input[name='name']").setValue("Василий Иванович");
+        $("input[name='phone']").setValue("+79990873456");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        $(byText("Успешно!")).waitUntil(visible, 15000);
     }
 }
